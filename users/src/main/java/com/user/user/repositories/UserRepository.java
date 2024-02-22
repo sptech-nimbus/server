a7da77c857648e5537478fe0fa19c755a522cb6a
KauanMO <kauaanmatheus@gmail.com>
2024-02-22 15:58:26 -0300

    Db: Campos email e senha sensiveis a maiusculo

--- users/src/main/java/com/user/user/repositories/UserRepository.java
+++ users/src/main/java/com/user/user/repositories/UserRepository.java
@@ -3,13 +3,11 @@ package com.user.user.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.user.user.models.user.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
     List<User> findByEmail(String email);
 
-    @Query(value = "SELECT * FROM User u WHERE BINARY u.email = ?1 AND BINARY u.password = ?2", nativeQuery = true)
     User findByEmailAndPassword(String email, String password);
 }
--- users/src/main/resources/database.sql
+++ users/src/main/resources/database.sql
@@ -4,7 +4,7 @@ CREATE TABLE User (
     user_id VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) COLLATE utf8_bin NOT NULL,
+    password VARCHAR(255) COLLATE utf8_bin NOT NULL,
     user_type VARCHAR(255) NOT NULL
 );
