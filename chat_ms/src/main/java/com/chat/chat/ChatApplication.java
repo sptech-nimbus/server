6b119a14dfee68004f618f2820d7b61beeb93cc8
KauanMO <kauaanmatheus@gmail.com>
2024-02-22 21:35:50 -0300

    Implementação inicial de chat_ms

---
+++ chat_ms/src/main/java/com/chat/chat/ChatApplication.java
@@ -0,0 +1,18 @@
+package com.chat.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
--- users_ms/src/main/java/com/user/user/controllers/UserController.java
+++ users_ms/src/main/java/com/user/user/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,5 +65,14 @@ public class UserController {
     @GetMapping
     public ResponseEntity<ResponseMessage> getAllUsers() {
         return ResponseEntity.ok(new ResponseMessage<List<UserRes>>(service.getUserResList(repo.findAll())));
+    }
+
+    @SuppressWarnings("rawtypes")
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseMessage> getById(@PathVariable String userId) {
+        User user = repo.findByUserId(userId);
+        UserRes userRes = service.convertUserToRes(user);
+
+        return ResponseEntity.ok(new ResponseMessage<UserRes>(userRes));
     }
 }
