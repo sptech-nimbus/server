94200346e245fc53f2bd96c35471edaffae2009c
KauanMO <kauaanmatheus@gmail.com>
2024-03-04 11:17:35 -0300

    Validações de registro de coach

--- users_ms/src/main/java/com/user/user/services/CoachService.java
+++ users_ms/src/main/java/com/user/user/services/CoachService.java
@@ -1,6 +1,7 @@
 package com.user.user.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +13,18 @@ import com.user.user.repositories.CoachRepository;
 
 @SuppressWarnings("rawtypes")
 @Service
-public class CoachService {
+public class CoachService extends PersonaService {
     @Autowired
     private CoachRepository repo;
 
     public ResponseEntity<ResponseMessage> register(CoachDTO dto, User user) {
         Coach newCoach = new Coach(dto);
-
         newCoach.setUser(user);
+
+        if (!checkPersonaCredencials(newCoach)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseMessage<>("Verifique suas credenciais de treinador"));
+        }
 
         repo.save(newCoach);
 
