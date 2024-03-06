382d2fb45a1fa487581514465a7d658ff39c3152
KauanMO <kauaanmatheus@gmail.com>
2024-03-06 15:03:20 -0300

    Identação teamDTO

--- users_ms/src/main/java/com/user/user/domains/team/TeamDTO.java
+++ users_ms/src/main/java/com/user/user/domains/team/TeamDTO.java
@@ -3,10 +3,9 @@ package com.user.user.domains.team;
 import com.user.user.domains.coach.Coach;
 
 public record TeamDTO(
-                String category,
-                String picture,
-                String local,
-                String name,
-                Coach coach) {
-
+        String category,
+        String picture,
+        String local,
+        String name,
+        Coach coach) {
 }
