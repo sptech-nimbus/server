345cd022c1fb30666fe006e1f3e8d2a8715d9b79
KauanMO <kauaanmatheus@gmail.com>
2024-03-26 15:18:17 -0300

    Busca pageada de treinos por time

--- events_ms/src/main/java/com/events/events/controllers/TrainingController.java
+++ events_ms/src/main/java/com/events/events/controllers/TrainingController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +31,13 @@ public class TrainingController {
     @PostMapping
     public ResponseEntity<ResponseMessage> registerTraining(@RequestBody TrainingDTO dto) {
         return service.register(dto);
+    }
+
+    // GET
+    @GetMapping("{teamId}/{page}/{elements}")
+    public ResponseEntity<ResponseMessage> getTrainingsPageByTeamId(@PathVariable UUID teamId,
+            @PathVariable Integer page, @PathVariable Integer elements) {
+        return service.getTrainingsPageByTeamId(teamId, page, elements);
     }
 
     // PUT
--- events_ms/src/main/java/com/events/events/services/TrainingService.java
+++ events_ms/src/main/java/com/events/events/services/TrainingService.java
@@ -4,6 +4,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -39,6 +42,17 @@ public class TrainingService {
         return ResponseEntity.status(200).body(new ResponseMessage<Training>(newTraining));
     }
 
+    public ResponseEntity<ResponseMessage> getTrainingsPageByTeamId(UUID teamId, Integer page, Integer elements) {
+        Pageable pageable = PageRequest.of(page, elements);
+
+        Page<Training> trainingsFound = repo.findAllByTeam(teamId, pageable);
+
+        if (!trainingsFound.hasContent())
+            return ResponseEntity.status(204).body(new ResponseMessage("Time sem treinos marcados"));
+
+        return ResponseEntity.status(200).body(new ResponseMessage<Page<Training>>(trainingsFound));
+    }
+
     public ResponseEntity<ResponseMessage> putTraining(UUID id, TrainingDTO dto) {
         if (!checkTrainingDontExists(dto, id))
             return ResponseEntity.status(409)
@@ -67,7 +81,6 @@ public class TrainingService {
         }
 
         return ResponseEntity.status(200).body(new ResponseMessage("Treino deletado"));
-
     }
 
     public Boolean checkTrainingDontExists(TrainingDTO dto) {
