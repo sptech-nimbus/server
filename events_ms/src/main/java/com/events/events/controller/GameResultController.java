707efbd289455d955b7bf862e1ea2e52b591a952
KauanMO <kauaanmatheus@gmail.com>
2024-04-02 10:34:25 -0300

    Busca de vitórias por time

--- events_ms/src/main/java/com/events/events/controller/GameResultController.java
+++ events_ms/src/main/java/com/events/events/controller/GameResultController.java
@@ -3,16 +3,19 @@ package com.events.events.controller;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.events.events.domain.coach.Coach;
 import com.events.events.domain.gameResult.GameResult;
 import com.events.events.domain.gameResult.GameResultDTO;
+import com.events.events.domain.graphs.WinsFromTeamDTO;
 import com.events.events.domain.responseMessage.ResponseMessage;
 import com.events.events.service.GameResultService;
 
@@ -29,6 +32,13 @@ public class GameResultController {
     @PostMapping
     public ResponseEntity<ResponseMessage<GameResult>> registerGameResult(@RequestBody GameResultDTO dto) {
         return service.register(dto);
+    }
+
+    // GET
+    @GetMapping("/graph/wins-by-team/{teamId}")
+    public ResponseEntity<ResponseMessage<WinsFromTeamDTO>> getWinsByTeam(@PathVariable UUID teamId,
+            @RequestParam Integer matches) {
+        return service.getWinsByTeam(teamId, matches);
     }
 
     // PATCH
