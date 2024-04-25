a97c01065944f5b74fdf6ef31e960346892a916e
zzzmiike <michael.silva@sptech.school>
2024-04-25 16:02:32 -0300

    feat: alterando entradas para o SQL Server na Azure

--- users_ms/src/main/java/com/user/user/domain/user/User.java
+++ users_ms/src/main/java/com/user/user/domain/user/User.java
@@ -17,8 +17,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "user")
-@Table(name = "user")
+@Entity(name = "app_user")
+@Table(name = "app_user")
 @Getter
 @Setter
 @NoArgsConstructor
